aa3273c17c87a7d55bff4de0d82166756762163d
yann <josterlau@gmail.com>
2020-07-30 17:00:04 +0800

    First commit

---
+++ Product.cs
@@ -0,0 +1,13 @@
+public class ProductViewHolder  {
+
+    public ProductViewHolder() {
+    }
+
+    public TextView getTxtProductNumber() {
+        return txtProductNumber;
+    }
+
+    public TextView getTxtProductName() {
+        return txtProductName;
+    }
+}
